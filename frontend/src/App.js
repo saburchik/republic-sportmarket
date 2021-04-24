d5a517294ef3482504b6f30a296cd2a42342e075
Yury <the.saburchik@gmail.com>
2021-04-24 23:34:33 +0700

    initial

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return <div className="app">
+    {/* Navbar */}
+    {/* SideDrawer */}
+    {/* Backdrop */}
+    {/* HomeScreen */}
+    {/* ProductScreen */}
+    {/* CartScreen */}
+  </div>;
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import './Navber.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            {/* logo */}
+            <div className="navbar__logo">
+                <h2>MERN Shopping Cart</h2>
+            </div>
+
+            {/* links */}
+            <ul className="navbar__links">
+                <li></li>
+            </ul>
+            {/* hamburger menu */}
+        </nav>
+    )
+}
+
+export default Navbar
