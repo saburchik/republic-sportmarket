73ca915e3296994e9c49230010b3a4c8f3f9e6de
Saburov <the.saburchik@gmail.com>
2021-11-08 22:39:01 +0700

    back styles

--- frontend/src/screens/HomeScreen/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import style from "./Homescreen.module.scss"
+import "./Homescreen.scss"
 import { React, useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 // Components
@@ -20,9 +20,9 @@ const HomeScreen = (props) => {
     const accordions = props.state.Categories.map(a => <Accordion key={a.id} title={a.title} subtitles={a.subtitles} />)
 
     // Loading, then checking for errors and finally drawing product cards
-    const drawsProducts = () => loading ? <h3 className={style.title}>Loading...</h3>
+    const drawsProducts = () => loading ? <h3 className="homescreen__title">Loading...</h3>
         : error
-            ? <h3 className={style.title}>{error}</h3>
+            ? <h3 className="homescreen__title">{error}</h3>
             : products.map(product => <Product
                 key={product._id}
                 productId={product._id}
@@ -38,21 +38,21 @@ const HomeScreen = (props) => {
         <main>
             <IntroSlider state={props.state} />
             <Advantages />
-            <article className={style.inner}>
-                <h2 className={style.title}>Ассортимент</h2>
-                <section className={style.product} >
-                    <div className={style.categories} >
-                        <h3 className={`${style.title} ${style.size_s}`}>Категории:</h3>
+            <article className="homescreen__inner">
+                <h2 className="homescreen__title">Ассортимент</h2>
+                <section className="homescreen__product" >
+                    <div className="homescreen__categories" >
+                        <h3 className="homescreen__title size_s">Категории:</h3>
                         <svg display="none">
                             <symbol id="chevron-down" viewBox="0 0 448 512">
                                 <path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
                             </symbol>
                         </svg>
-                        <ul className={style.accordion}>
+                        <ul className="homescreen__accordion">
                             {accordions}
                         </ul>
                     </div>
-                    <ul className={style.cards}>
+                    <ul className="homescreen__cards">
                         <svg display="none">
                             <symbol id="play-solid" viewBox="0 0 448 512">
                                 <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path>
