85e95f2fac7f561f4e71e9ce30c3ff1864266312
Saburov <the.saburchik@gmail.com>
2021-11-08 19:56:34 +0700

    add disable for btn & refactoring code/styles

--- frontend/src/screens/ProductScreen/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen/ProductScreen.jsx
@@ -1,5 +1,5 @@
-import './ProductScreen.scss';
-import { useState, useEffect } from "react";
+import style from './ProductScreen.module.scss';
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from 'react-router-dom'
 
@@ -52,79 +52,89 @@ const ProductScreen = ({ match, history }) => {
         return iconStatusColor.join(' ')
     }
 
+    let isTrue = false
+    const disableBtn = () => {
+        if (product.countInStock === 0) {
+            isTrue = true
+        } else {
+            return <div className={style.quantity}>
+                <p>Количество:</p>
+                <select className={style.select} value={qty} onChange={(e) => setQty(e.target.value)}>
+                    {[...Array(product.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>{x + 1}</option>
+                    ))}
+                </select>
+            </div>
+        }
+    }
+
+    let addGrey = () => {
+        if (product.countInStock === 0) {
+            return { background: '#d0d0d0', color: '#9a9a9a', border: '1px solid #9a9a9a' }
+        }
+    }
 
 
     return (
-        <div className="productscreen">
+        <div>
             {loading ? <h2>Loading...</h2> : error ? <h2>{error}</h2> : (
                 <>
                     <Advantages />
-                    <div className="product__about">
-                        <div className="description__inner">
-                            <div className="block_img">
-                                <span className={checkStatus()}>{product.status}</span>
-                                <img
-                                    src={product.imageUrl}
-                                    alt={product.name}
-                                />
+                    <div className={style.inner}>
+                        <div className={style.images}>
+                            <span className={checkStatus()}>{product.status}</span>
+                            <img
+                                src={product.imageUrl}
+                                alt={product.name}
+                            />
+                        </div>
+
+                        <div className={style.information}>
+                            <header>
+                                <p className={style.name}>{product.name}</p>
+                                <p className={style.price}>
+                                    {product.price} ₽
+                                    <strike className={
+                                        product.oldPrice === 0 ? "old-price undefined" : "old-price"
+                                    }>{product.oldPrice}
+                                    </strike>
+                                </p>
+                            </header>
+
+                            <div className={style.contains}>
+                                <p className={style.article}>артикул: <strong>{product.article}</strong></p>
+                                <p className={style.description}>
+                                    <strong>Описание:</strong>
+                                    {product.description}
+                                </p>
+                                <button className={style.btn}>Размераня сетка</button>
+                                <div className={style.status}>
+                                    <p className={style.article}>статус:
+                                        <strong> {product.countInStock > 0 ? "В наличии" : "Нет в наличии"}</strong>
+                                    </p>
+                                    {disableBtn()}
+                                </div>
                             </div>
 
-                            <div className="product__information">
-                                <div>
-                                    <p className="productscreen__title">{product.name}</p>
-                                </div>
-
-                                <div className="inner__contains">
-                                    <p className="product__article">артикул: <strong>{product.article}</strong></p>
-                                    <p className="productscreen__price">
-                                        {product.price} ₽
-                                        <strike className={
-                                            product.oldPrice === 0 ? "old-price undefined" : "old-price"
-                                        }>{product.oldPrice}
-                                        </strike>
-                                    </p>
-                                    <p className="description description__product">
-                                        <strong>Описание:</strong>
-                                        {product.description}
-                                    </p>
-                                    <button className="productscreen__btn">Размераня сетка</button>
-                                    <div className="grid__quantity">
-                                        <p className="product__article">статус:
-                                            <strong> {product.countInStock > 0 ? "В наличии" : "Нет в наличии"}</strong>
-                                        </p>
-                                        <div className="select__product">
-                                            <p className="count__qty">Количество:</p>
-                                            <select className="count__select" value={qty} onChange={(e) => setQty(e.target.value)}>
-                                                {[...Array(product.countInStock).keys()].map((x) => (
-                                                    <option key={x + 1} value={x + 1}>{x + 1}</option>
-                                                ))}
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="add__cart">
-                                    {/* <Link to="/"> */}
-                                    <button
-                                        className="productscreen__btn color"
-                                        type="button"
-                                        onClick={addToCartHandler}
-                                    >В корзину</button>
-                                    {/* </Link> */}
-                                </div>
-                            </div>
+                            <button
+                                className={`${style.btn} ${style.color}`}
+                                type="button"
+                                onClick={addToCartHandler}
+                                disabled={isTrue}
+                                style={addGrey()}
+                            >В корзину</button>
                         </div>
                     </div>
-                    <div className="reviews" style={{ padding: '1rem' }}>
-                        <h3 className="title title__size-sl">Отзывы</h3>
-                        <div className="review__client">
-                            <strong className="title client__name">Saburchik:</strong>
-                            <p className="description client__text">Отличный магазин, доставка до Красноярска как и заявлена 4 дня, постоянно есть обратная связь. Доска без косяков, что хотел, то и получил, приятно когда работают профессионалы.</p>
-                            <strong className="client__data">02.03.2021 | 19:42</strong>
+                    <div className={style.reviews}>
+                        <h3 className={style.title}>Отзывы</h3>
+                        <div className={style.client}>
+                            <strong className={`${style.title} ${style.client__name}`}>Saburchik:</strong>
+                            <p className={`${style.description} ${style.description__text}`}>Отличный магазин, доставка до Красноярска как и заявлена 4 дня, постоянно есть обратная связь. Доска без косяков, что хотел, то и получил, приятно когда работают профессионалы.</p>
+                            <strong className={style.date}>02.03.2021 | 19:42</strong>
                         </div>
-                        <p className="review__alert">
-                            Отзывы могут оставлять только <Link className="registration__link" to="/">зарегистрированные</Link> пользователи. <br />
-                            <Link className="registration__link" to="/" >Авторизоваться и оставить отзыв</Link>
+                        <p className={style.alert}>
+                            Отзывы могут оставлять только <Link className={style.link} to="/">зарегистрированные</Link> пользователи. <br />
+                            <Link className={style.link} to="/" >Авторизоваться и оставить отзыв</Link>
                         </p>
                     </div>
                 </>
