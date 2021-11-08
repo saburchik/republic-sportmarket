ebbe220d785e167642e5e1b0649e86155147b8ef
Saburov <the.saburchik@gmail.com>
2021-11-08 20:03:49 +0700

    add disable for btn & refactoring code/styles AGAIN

--- frontend/src/screens/ProductScreen/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen/ProductScreen.jsx
@@ -1,5 +1,5 @@
 import style from './ProductScreen.module.scss';
-import React, { useState, useEffect } from "react";
+import { React, useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from 'react-router-dom'
 
