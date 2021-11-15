b2188b3b43afafb83543e86639f03615aae26610
Saburov <the.saburchik@gmail.com>
2021-11-15 20:16:56 +0700

    refactoring styles

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,68 +1,106 @@
-import './styles/Footer.scss';
-import { Link } from 'react-router-dom';
-import logo from './../assets/footer-logo.png';
+import React from "react"
+import style from "./styles/Footer.module.scss"
+import { Link } from "react-router-dom"
+import logo from "./../assets/republic.png"
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="footer__inner">
-                <div className="footer__item">
-                    <div className="footer__header">
-                        <Link to="/">
-                            <img className="footer__logo" src={logo} alt="Логотип" />
-                        </Link>
-                    </div>
-                    <div className="footer__contacts">
-                        <b className="footer__title">
-                            Время работы службы поддержки:
-                            <i>ПН-ПТ с 09:00 до 21:00</i>
-                        </b>
-                        <b className="footer__title">
-                            Горячая линия:
-                            <i>+7 800 5555 585</i>
-                        </b>
-                        <b className="footer__title">
-                            Email:
-                            <i>republic-store@gmail.com</i>
-                        </b>
-                        <Link to="/" className="footer__link">Обратная связь</Link>
-                    </div>
+        <footer className={style.inner}>
+            <address className={style.item}>
+                <div className={style.header}>
+                    <Link to="/">
+                        <img className={style.logo} src={logo} alt="Логотип" />
+                    </Link>
                 </div>
-                <div className="footer__item">
-                    <div className="footer__header">
-                        <b className="footer__title">Информация:</b>
-                    </div>
-                    <div className="links__wrapper">
-                        <Link to="/" className="footer__link">О компании</Link>
-                        <Link to="/" className="footer__link">Частые вопросы [FAQ]</Link>
-                        <Link to="/" className="footer__link">Работа у нас</Link>
-                        <Link to="/" className="footer__link">Правовая информация</Link>
-                        <Link to="/" className="footer__link">Сотрудничество</Link>
-                    </div>
+                <div className={style.contacts}>
+                    <strong className={style.title}>
+                        Время работы службы поддержки:
+                        <em>ПН-ПТ с 09:00 до 21:00</em>
+                    </strong>
+                    <strong className={style.title}>
+                        Горячая линия:
+                        <a href="tel:88005555585">+7 800 5555 585</a>
+                    </strong>
+                    <strong className={style.title}>
+                        Email:
+                        <a href="mailto:republic-store@gmail.com">republic.store@gmail.com</a>
+                    </strong>
+                    <Link to="/" className={style.link}>Обратная связь</Link>
                 </div>
-                <div className="footer__item">
-                    <div className="footer__header">
-                        <b className="footer__title">Покупателем:</b>
-                    </div>
-                    <div className="links__wrapper">
-                        <Link className="footer__link" to="/">Условия продаж</Link>
-                        <Link className="footer__link" to="/">Оплата и доставка</Link>
-                        <Link className="footer__link" to="/">Гарантия и возврат</Link>
-
-                        <div className="links__media">
-                            <Link className="footer__icon" to="/" target="_blank"><i className="fab fa-vk" /></Link>
-                            <Link className="footer__icon" to="/" target="_blank"><i className="fab fa-youtube" /></Link>
-                            <Link className="footer__icon" to="/" target="_blank"><i className="fab fa-telegram" /></Link>
-                            <Link className="footer__icon" to="/" target="_blank"><i className="fab fa-instagram" /></Link>
-                        </div>
-                    </div>
+            </address>
+            <section className={style.item}>
+                <div className={style.header}>
+                    <h4 className={style.title}>Информация</h4>
                 </div>
-                <p className="footer__rights">
-                    2020 – 2021, © ИМ Республика.
-                    Все права защищены. Информация, размещенная на данной странице,
-                    не является публичной офертой.
-                </p>
-            </div>
+                <ul className={style.wrapper}>
+                    <li><Link className={style.link} to="/">О компании</Link></li>
+                    <li><Link className={style.link} to="/">Частые вопросы [FAQ]</Link></li>
+                    <li><Link className={style.link} to="/">Работа у нас</Link></li>
+                    <li><Link className={style.link} to="/">Правовая информация</Link></li>
+                    <li><Link className={style.link} to="/">Сотрудничество</Link></li>
+                </ul>
+            </section>
+            <section className={style.item}>
+                <div className={style.header}>
+                    <h4 className={style.title}>Покупателем</h4>
+                </div>
+                <ul className={style.wrapper}>
+                    <li><Link className={style.link} to="/">Условия продаж</Link></li>
+                    <li><Link className={style.link} to="/">Оплата и доставка</Link></li>
+                    <li><Link className={style.link} to="/">Гарантия и возврат</Link></li>
+                    <li>
+                        <ul className={style.media}>
+                            <svg display="none">
+                                <symbol id="vk-brands" viewBox="0 0 576 512">
+                                    <path d="M545 117.7c3.7-12.5 0-21.7-17.8-21.7h-58.9c-15 0-21.9 7.9-25.6 16.7 0 0-30 73.1-72.4 120.5-13.7 13.7-20 18.1-27.5 18.1-3.7 0-9.4-4.4-9.4-16.9V117.7c0-15-4.2-21.7-16.6-21.7h-92.6c-9.4 0-15 7-15 13.5 0 14.2 21.2 17.5 23.4 57.5v86.8c0 19-3.4 22.5-10.9 22.5-20 0-68.6-73.4-97.4-157.4-5.8-16.3-11.5-22.9-26.6-22.9H38.8c-16.8 0-20.2 7.9-20.2 16.7 0 15.6 20 93.1 93.1 195.5C160.4 378.1 229 416 291.4 416c37.5 0 42.1-8.4 42.1-22.9 0-66.8-3.4-73.1 15.4-73.1 8.7 0 23.7 4.4 58.7 38.1 40 40 46.6 57.9 69 57.9h58.9c16.8 0 25.3-8.4 20.4-25-11.2-34.9-86.9-106.7-90.3-111.5-8.7-11.2-6.2-16.2 0-26.2.1-.1 72-101.3 79.4-135.6z" />
+                                </symbol>
+                                <symbol id="youtube-brands" viewBox="0 0 576 512">
+                                    <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" />
+                                </symbol>
+                                <symbol id="telegram-brands" viewBox="0 0 496 512">
+                                    <path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm121.8 169.9l-40.7 191.8c-3 13.6-11.1 16.9-22.4 10.5l-62-45.7-29.9 28.8c-3.3 3.3-6.1 6.1-12.5 6.1l4.4-63.1 114.9-103.8c5-4.4-1.1-6.9-7.7-2.5l-142 89.4-61.2-19.1c-13.3-4.2-13.6-13.3 2.8-19.7l239.1-92.2c11.1-4 20.8 2.7 17.2 19.5z" />
+                                </symbol>
+                                <symbol id="instagram-brands" viewBox="0 0 448 512">
+                                    <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+                                </symbol>
+                            </svg>
+                            <li>
+                                <Link className={style.icon} to="/">
+                                    <svg>
+                                        <use xlinkHref="#vk-brands" />
+                                    </svg>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link className={style.icon} to="/">
+                                    <svg>
+                                        <use xlinkHref="#youtube-brands" />
+                                    </svg>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link className={style.icon} to="/">
+                                    <svg>
+                                        <use xlinkHref="#telegram-brands" />
+                                    </svg>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link className={style.icon} to="/">
+                                    <svg>
+                                        <use xlinkHref="#instagram-brands" />
+                                    </svg>
+                                </Link>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </section>
+            <p className={style.rights}>
+                2020 – 2021, © ИМ Республика.
+                Все права защищены. Информация, размещенная на данной странице,
+                не является публичной офертой.
+            </p>
         </footer>
     )
 }
--- frontend/src/components/SideDrawer.jsx
+++ frontend/src/components/SideDrawer.jsx
@@ -1,60 +1,58 @@
-import "./styles/SideDrawer.scss";
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import logo from './../assets/republic.png';
+import style from "./styles/SideDrawer.module.scss"
+import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
+import logo from "./../assets/republic.png"
 
 const SideDrawer = ({ show, click }) => {
-
-    const sideDrawerClass = ["sidedrawer"]
-
+    const sideDrawerClass = [style.sidedrawer]
     if (show) {
-        sideDrawerClass.push("show")
+        sideDrawerClass.push(style.show)
     }
 
     const cart = useSelector(state => state.cart)
     const { cartItems } = cart
+    const getCartCount = () => cartItems.reduce((qty, item) => qty + Number(item.qty), 0)
 
-    const getCartCount = () => {
-        return cartItems.reduce((qty, item) => qty + Number(item.qty), 0)
-    }
+    return (
+        <nav className={sideDrawerClass.join(" ")}>
+            <ul className={style.header}>
+                <li>
+                    <Link to="/"><img className={style.logo} src={logo} alt="Логотип Республики" /></Link>
+                </li>
+                <li>
+                    <Link className={style.cart} to="/cart" onClick={click}>
+                        <span className={style.cart__num}>{getCartCount()}</span>
+                        <svg className={style.cart__logo} viewBox="0 0 576 512">
+                            <path d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z" />
+                        </svg>
+                    </Link>
+                </li>
+            </ul>
 
-    return <div className={sideDrawerClass.join(" ")}>
-        <ul className="sidedrawer__links">
-            <li className="navbar__logo">
-                {/* <Link to="/" onClick={click}>
-                    <img className="logotype" src={logo} alt="Republic-logo" />
-                </Link> */}
-                <Link to="/">
-                    <img className="navbar__logo logotype" src={logo} alt="Republic-logo" />
-                </Link>
-            </li>
-            <li>
-                <Link className="cart__link word" to="/cart" onClick={click}>
-                    <i className="fas fa-shopping-cart"></i>
-                    <span className="cartlogo__badge">{getCartCount()}</span>
-                </Link>
-            </li>
-        </ul>
-
-        <ul className="search--mobile">
-            <li>
-                <Link className="cart__link" to="/login" onClick={click}>
-                    Вход
-                </Link>
-            </li>
-            <li>
-                <Link className="navbar__helped" to="/">
-                    Помощь
-                </Link>
-            </li>
-            <li>
-                <form>
-                    <input className="input search__input" placeholder="Поиск" />
-                    <button className="input__btn search__btn"><i className="fas fa-search"></i></button>
-                </form>
-            </li>
-        </ul>
-    </div>
+            <ul className={style.items}>
+                <li>
+                    <Link className={style.links} to="/login" onClick={click}>
+                        Вход
+                    </Link>
+                </li>
+                <li>
+                    <Link className={style.links} to="/">
+                        Помощь
+                    </Link>
+                </li>
+                <li>
+                    <form>
+                        <input className={style.input} placeholder="Поиск" />
+                        <button className={style.btn}>
+                            <svg viewBox="0 0 512 512">
+                                <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" />
+                            </svg>
+                        </button>
+                    </form>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default SideDrawer
